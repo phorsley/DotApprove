310c4576e03fa7a0e499a2da5ec054eb4c08b14f
Louis Pilfold <louis@lpil.uk>
2014-09-30 15:34:35 +0100

    Cleanup monthly plugin, remove excess newlines

--- _plugins/monthly_archive_plugin.rb
+++ _plugins/monthly_archive_plugin.rb
@@ -15,10 +15,11 @@
 # site configuration file. 'path' is default null.
 
 module Jekyll
-
+  # Monthly blog archive generator
   module MonthlyArchiveUtil
     def self.archive_base(site)
-      site.config['monthly_archive'] && site.config['monthly_archive']['path'] || ''
+      site.config['monthly_archive'] &&
+        site.config['monthly_archive']['path'] || ''
     end
   end
 
@@ -26,26 +27,24 @@ module Jekyll
   class MonthlyArchiveGenerator < Generator
     def generate(site)
       posts_group_by_year_and_month(site).each do |ym, list|
-        site.pages << MonthlyArchivePage.new(site, MonthlyArchiveUtil.archive_base(site),
-                                             ym[0], ym[1], list)
+        site.pages << MonthlyArchivePage.new(
+          site,
+          MonthlyArchiveUtil.archive_base(site),
+          ym[0],
+          ym[1],
+          list
+        )
       end
     end
 
     def posts_group_by_year_and_month(site)
       site.posts.each.group_by { |post| [post.date.year, post.date.month] }
     end
-
   end
 
   # Actual page instances
   class MonthlyArchivePage < Page
-
-    ATTRIBUTES_FOR_LIQUID = %w[
-      year,
-      month,
-      date,
-      content
-    ]
+    ATTRIBUTES_FOR_LIQUID = %w(year month date content)
 
     def initialize(site, dir, year, month, posts)
       @site = site
@@ -54,45 +53,47 @@ module Jekyll
       @month = month
       @archive_dir_name = '%04d/%02d' % [year, month]
       @date = Date.new(@year, @month)
-      @layout =  site.config['monthly_archive'] && site.config['monthly_archive']['layout'] || 'monthly_archive'
+      @layout =
+        site.config['monthly_archive'] &&
+        site.config['monthly_archive']['layout'] ||
+        'monthly_archive'
       self.ext = '.html'
       self.basename = 'index'
       self.content = <<-EOS
 {% for post in page.posts reversed %}{% include post-summary.html %}
 {% endfor %}
-      EOS
+EOS
       self.data = {
         'layout' => @layout,
         'type' => 'archive',
         'title' => "Monthly archive for #{@year}/#{@month}",
         'posts' => posts,
-          'url' => File.join('/',
-                             MonthlyArchiveUtil.archive_base(site),
-                             @archive_dir_name, 'index.html')
+        'url' => File.join('/',
+                           MonthlyArchiveUtil.archive_base(site),
+                           @archive_dir_name, 'index.html')
       }
     end
 
     def render(layouts, site_payload)
       payload = {
-        'page' => self.to_liquid,
+        'page' => to_liquid,
         'paginator' => pager.to_liquid
       }.merge(site_payload)
       do_layout(payload, layouts)
     end
 
-    def to_liquid(attr = nil)
-      self.data.merge({
-        'content' => self.content,
-        'date' => @date,
-        'month' => @month,
-        'year' => @year
-      })
+    def to_liquid(*)
+      data.merge(
+          'content' => content,
+          'date' => @date,
+          'month' => @month,
+          'year' => @year
+        )
     end
 
     def destination(dest)
       File.join('/', dest, @dir, @archive_dir_name, 'index.html')
     end
-
   end
 end
 
